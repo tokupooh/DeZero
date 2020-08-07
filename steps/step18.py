b3cb39433ec0e5c3e27807e52c2d5a177b83554a
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-07 07:54:14 +0000

    add nograd mode

---
+++ steps/step18.py
@@ -0,0 +1,136 @@
+import weakref
+import numpy as np
+import contextlib
+
+
+class Config():
+    enable_backprop = True
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+
+class Variable():
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} is not supported.')
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def cleargrad(self):
+        self.grad = None
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        # functionのオブジェクトidが同じものを除くために必要
+        # それで、ダブルカウントを避ける
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+
+            if not isinstance(gxs, tuple):
+                gxs = (gxs, )
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx  # do not use inplace-operator
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+            if not retain_grad:
+                # これで末端以外のものはアウトプットであることから、削除される
+                for y in f.outputs:
+                    y().grad = None
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys, )
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+
+            for output in outputs:
+                output.set_creator(self)
+
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x**2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+def square(x):
+    return Square()(x)
+
+
+if __name__ == '__main__':
+
+    with no_grad():
+        x = Variable(np.array(2.0))
+        y = square(x)
+        y.backward()
+        print(y.grad)  # throw attribute error
