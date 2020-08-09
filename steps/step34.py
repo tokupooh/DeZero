b1933419781c69551e47162ac5ccca009d77eeef
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-09 07:39:59 +0000

    check sin result

---
+++ steps/step34.py
@@ -0,0 +1,31 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+from dezero import Variable
+
+import matplotlib.pyplot as plt
+import dezero.functions as F
+
+if __name__ == '__main__':
+    x = Variable(np.linspace(-7, 7, 200))
+    y = F.sin(x)
+
+    y.backward(create_graph=True)
+
+    logs = [y.data]
+
+    for i in range(3):
+        logs.append(x.grad.data)
+        gx = x.grad
+        x.cleargrad()
+        gx.backward(create_graph=True)
+
+    labels = ["y=sin(x)", "y'", "y''", "y'''"]
+
+    for i, v in enumerate(logs):
+        plt.plot(x.data, logs[i], label=labels[i])
+
+    plt.legend(loc='lower right')
+    plt.savefig('sin_diff_logs.png')
