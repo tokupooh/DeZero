02aa9a7117f76873aaae1dbf9729e36d4c9b437a
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-11 06:59:21 +0000

    add Adam class

--- dezero/optimizers.py
+++ dezero/optimizers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 # =============================================================================
@@ -108,3 +109,42 @@ class AdaDelta(Optimizer):
         msdx *= rho
         msdx += (1 - rho) * dx * dx
         param.data -= dx
+
+
+class Adam(Optimizer):
+    def __init__(self, alpha=0.001, beta1=0.9, beta2=0.999, eps=1e-8):
+        super().__init__()
+
+        self.t = 0
+        self.alpha = alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.ms = {}  # moving-average gradient
+        self.vs = {}  # moving-average gradient squared
+
+    def update(self, *args, **kwargs):
+        self.t += 1
+        super().update(*args, **kwargs)
+
+    @property
+    def lr(self):
+        fix1 = 1.0 - math.pow(self.beta1, self.t)
+        fix2 = 1.0 - math.pow(self.bata2, self.t)
+
+        return self.alpha * math.sqrt(fix2) / fix1
+
+    def update_one(self, param):
+
+        key = id(param)
+        if key not in self.ms:
+            self.ms[key] = np.zeros_like(param.data)
+            self.vs[key] = np.zeros_like(param.data)
+
+        m, v = self.ms[key], self.vs[key]
+        beta1, beta2, eps = self.beta1, self.beta2, self.eps
+        grad = param.grad.data
+
+        m += (1 - beta1) * (grad - m)
+        v += (1 - beta2) * (grad * grad - v)
+        param.data -= self.lr * m / np.sqrt(v) + eps
