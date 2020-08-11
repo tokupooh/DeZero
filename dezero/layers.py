0a12b5031ccc0c8e5630ccb168fd56614017e595
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-11 05:01:58 +0000

    add Linear layer

--- dezero/layers.py
+++ dezero/layers.py
@@ -1,5 +1,10 @@
-from dezero.core import Parameter
 import weakref
+
+import numpy as np
+
+import dezero.functions as F
+from dezero.core import Parameter
+
 # =============================================================================
 # Layer (base class)
 # =============================================================================
@@ -38,3 +43,39 @@ class Layer():
     def cleargrads(self):
         for param in self.params():
             param.cleargrad()
+
+
+# =============================================================================
+# Linear / Conv2d / Deconv2d
+# =============================================================================
+
+
+class Linear(Layer):
+    def __init__(self, out_size, nobias=False, dtype=np.float32, in_size=None):
+        super().__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.dtype = dtype
+
+        self.W = Parameter(None, name='W')
+        if self.in_size is not None:
+            self._init_W()
+
+        if nobias:
+            self.b = None
+        else:
+            self.b = Parameter(np.zeros(self.out_size, dtype=self.dtype),
+                               name='b')
+
+    def _init_W(self):
+        I, O = self.in_size, self.out_size
+        W_data = np.random.randn(I, O).astype(self.dtype) * np.sqrt(1 / I)
+        self.W.data = W_data
+
+    def forward(self, x):
+        if self.W.data is None:
+            self.in_size = x.shape[1]
+            self._init_W()
+
+        y = F.linear(x, self.W, self.b)
+        return y
