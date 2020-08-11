c8f55066f239ae3a2aab71e6887c717b7a4ccba7
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-11 05:15:46 +0000

    add an efficient sigmoid func

--- dezero/functions.py
+++ dezero/functions.py
@@ -238,6 +238,21 @@ def sigmoid_simple(x):
     return y
 
 
+class Sigmoid(Function):
+    def forward(self, x):
+        y = np.tanh(x * 0.5) * 0.5 + 0.5
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
+
+
 # =============================================================================
 # loss function:
 # mean_squared_error / softmax_cross_entropy /
