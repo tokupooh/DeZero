1309a938cc1004a9c828b9f6ed860708cee16ed6
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-11 05:34:18 +0000

    fix __init__.py

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -17,7 +17,12 @@ else:
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
-    from dezero.core import Parameter
+    from dezero.core import Config
+    from dezero.layers import Layer
+    from dezero.models import Model
+
     import dezero.functions
     import dezero.layers
+    import dezero.utils
+
 setup_variable()
