d824e0d375a56484236dc0a4d9accc09331f03ed
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-11 06:20:36 +0000

    add MomentumSGD class

--- dezero/optimizers.py
+++ dezero/optimizers.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 # =============================================================================
 # Optimizer (base class)
 # =============================================================================
@@ -38,3 +41,21 @@ class SGD(Optimizer):
 
     def update_one(self, param):
         param.data -= self.lr * param.grad.data
+
+
+class MomentumSGD(Optimizer):
+    def __init__(self, lr=0.01, momentum=0.9):
+        super().__init__()
+        self.lr = lr
+        self.momentum = momentum
+        self.vs = {}
+
+    def update_one(self, param):
+        v_key = id(param)
+        if v_key not in self.vs:
+            self.vs[v_key] = np.zeros_like(param.data)
+
+        v = self.vs[v_key]
+        v *= self.momentum
+        v - self.lr * param.grad.data
+        param.data += v
