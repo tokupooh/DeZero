abc056edcd2e9987158ea82fe96b3b68974e71ed
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-13 09:31:54 +0000

    add Dataset class

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -1,5 +1,40 @@
 import numpy as np
 
+# =============================================================================
+# Base class
+# =============================================================================
+
+
+class Dataset():
+    def __init__(self, train=True, transform=None, target_transform=None):
+        self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if self.transform is None:
+            self.transform = lambda x: x
+
+        if self.target_transform is None:
+            self.target_transform = lambda x: x
+
+        self.data = None
+        self.label = None
+        self.prepare()
+
+    def _getitem__(self, index):
+        assert np.isscalar(index)
+        if self.label is None:
+            return self.transform(self.data[index]), None
+        else:
+            return self.transform(self.data[index]), self.target_transform(
+                self.label[index])
+
+    def __len__(self):
+        return len(self.data)
+
+    def prepare(self):
+        pass
+
 
 # =============================================================================
 # Toy datasets
@@ -27,3 +62,8 @@ def get_spiral(train=True):
     x = x[indices]
     t = t[indices]
     return x, t
+
+
+class Spiral(Dataset):
+    def prepare(self):
+        self.data, self.label = get_spiral(self.train)
