4f7c306729530302501badb4ea168f5b8a08c394
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2020-08-30 08:12:34 +0000

    add dataloader

--- dezero/dataloaders.py
+++ dezero/dataloaders.py
@@ -57,3 +57,23 @@ class DataLoader():
 
     def next(self):
         return self.__next__()
+
+
+class SeqDataLoader(DataLoader):
+    def __init__(self, dataset, batch_size):
+        super().__init__(dataset=dataset, batch_size=batch_size)
+
+    def __next__(self):
+        if self.iteration >= self.max_iter:
+            self.reset()
+            raise StopIteration
+
+        jump = self.data_size // self.batch_size
+        batch_index = [(i * jump + self.iteration) % self.data_size
+                       for i in range(self.batch_size)]
+        batch = [self.dataset[i] for i in batch_index]
+        x = np.array([example[0] for example in batch])
+        t = np.array([example[1] for example in batch])
+
+        self.iteration += 1
+        return x, t
